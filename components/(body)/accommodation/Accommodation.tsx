57e5dd9f2bdcc6c92da6c23c80ddbe1a53af482d
oliver ahmed <oliverahmed000@gmail.com>
2025-10-28 17:08:01 +0600

    accommodation animation off in mobile version added

--- components/(body)/accommodation/Accommodation.tsx
+++ components/(body)/accommodation/Accommodation.tsx
@@ -26,8 +26,8 @@ const Accommodation = () => {
                 style={{ backgroundImage: `url(${bg})` }}
               >
                 {" "}
-                <div className="w-full relative  h-full group flex flex-col items-center justify-center  ">
-                  <div className="bg-yellow-300/80  capitalize  w-full md:group-hover:-translate-y-[93px] lg:group-hover:-translate-y-[131px] md:mt-23 h-10 group-hover:transition-transform duration-500 ease-in-out flex items-center justify-center ">
+                <div className="w-full relative h-full group flex flex-col items-center justify-center gap-y-5  ">
+                  <div className="bg-yellow-300/80  capitalize  w-full md:group-hover:-translate-y-[93px] lg:group-hover:-translate-y-[131px] md:mt-23 md:py-1.5 py-1 group-hover:transition-transform duration-500 ease-in-out flex items-center justify-center ">
                     <p className=" flex items-center  justify-center">
                       {" "}
                       {title}{" "}
@@ -35,7 +35,7 @@ const Accommodation = () => {
                   </div>
 
                   {/* ------- book now & read more btn ------- */}
-                  <div className=" opacity-0 md:group-hover:opacity-100 md:scale-0 md:group-hover:scale-100 md:group-hover:transition-all duration-500 ease-in-out md:flex flex-col md:gap-y-1 md:mb-8 md:origin-center   ">
+                  <div className=" md:opacity-0 md:group-hover:opacity-100 md:scale-0 md:group-hover:scale-100 md:group-hover:transition-all duration-500 ease-in-out flex gap-x-3  md:flex-col md:gap-y-1 md:mb-8 md:origin-center text-sm   ">
                     <Link href="https://book-directonline.com/properties/AussieRestMotelDirect?locale=en&items[0][adults]=2&items[0][children]=0&items[0][infants]=0&currency=AUD&checkInDate=2025-10-25&checkOutDate=2025-10-26&trackPage=yes">
                       <div className="bg-black/70 hover:bg-black duration-500 px-6 py-2 ">
                         <p className="text-white capitalize">book now</p>
