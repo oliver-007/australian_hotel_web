6aff861993ff78716832d96fb616da32ac5efdd6
oliver ahmed <oliverahmed000@gmail.com>
2025-10-28 16:53:59 +0600

    accommodation animation for lg screen added

--- components/(body)/accommodation/Accommodation.tsx
+++ components/(body)/accommodation/Accommodation.tsx
@@ -27,13 +27,15 @@ const Accommodation = () => {
               >
                 {" "}
                 <div className="w-full relative  h-full group flex flex-col items-center justify-center  ">
-                  <div className="bg-yellow-300/80 lg:py-3 capitalize  w-full group-hover:-translate-y-[93px] mt-23 h-10 group-hover:transition-transform duration-500 ease-in-out flex items-center justify-center ">
+                  <div className="bg-yellow-300/80  capitalize  w-full md:group-hover:-translate-y-[93px] lg:group-hover:-translate-y-[131px] md:mt-23 h-10 group-hover:transition-transform duration-500 ease-in-out flex items-center justify-center ">
                     <p className=" flex items-center  justify-center">
                       {" "}
                       {title}{" "}
                     </p>
                   </div>
-                  <div className=" opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100 group-hover:transition-all duration-500 ease-in-out flex flex-col gap-y-1 mb-8 origin-center  ">
+
+                  {/* ------- book now & read more btn ------- */}
+                  <div className=" opacity-0 md:group-hover:opacity-100 md:scale-0 md:group-hover:scale-100 md:group-hover:transition-all duration-500 ease-in-out md:flex flex-col md:gap-y-1 md:mb-8 md:origin-center   ">
                     <Link href="https://book-directonline.com/properties/AussieRestMotelDirect?locale=en&items[0][adults]=2&items[0][children]=0&items[0][infants]=0&currency=AUD&checkInDate=2025-10-25&checkOutDate=2025-10-26&trackPage=yes">
                       <div className="bg-black/70 hover:bg-black duration-500 px-6 py-2 ">
                         <p className="text-white capitalize">book now</p>
